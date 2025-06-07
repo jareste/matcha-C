c5745f9ad005d4ba3c8a7875c49b82e7f75bdb19
Joan Aresté Gonzalez <124678884+jareste@users.noreply.github.com>
2025-06-07 09:17:17 +0000

    added logic for handling routes properly

--- server/srcs/router/router.c
+++ server/srcs/router/router.c
@@ -3,7 +3,6 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
-
 #include "../log/log_api.h"
 #include "../parse/config_file.h"
 #include "../../inc/ft_malloc.h"
@@ -11,6 +10,90 @@
 #include "router_api.h"
 
 static route_entry_t* m_routes = NULL;
+
+static const char* m_http_code_to_status_text(HTTP_response_code_t code)
+{
+    switch (code)
+    {
+        case CODE_200_OK: return "OK";
+        case CODE_201_CREATED: return "Created";
+        case CODE_204_NO_CONTENT: return "No Content";
+        case CODE_400_BAD_REQUEST: return "Bad Request";
+        case CODE_404_NOT_FOUND: return "Not Found";
+        case CODE_405_METHOD_NOT_ALLOWED: return "Method Not Allowed";
+        case CODE_500_INTERNAL_SERVER_ERROR: return "Internal Server Error";
+        case CODE_503_SERVICE_UNAVAILABLE: return "Service Unavailable";
+        default: return "Unknown";
+    }
+}
+
+int router_http_generate_response(int fd, HTTP_response_code_t code, const char* body)
+{
+    char body_buf[128];
+    char header[512];
+    size_t body_len;
+    const char* status_text;
+    int header_len;
+
+    if (code == CODE_204_NO_CONTENT)
+    {
+        snprintf(header, sizeof(header), "HTTP/1.1 204 No Content\r\nConnection: close\r\n\r\n");
+
+        send(fd, header, strlen(header), 0);
+    }
+    else if (body)
+    {
+        status_text = m_http_code_to_status_text(code);
+        body_len = strlen(body);
+
+        header_len = snprintf(header, sizeof(header),
+            "HTTP/1.1 %d %s\r\n"
+            "Content-Type: application/json\r\n"
+            "Access-Control-Allow-Origin: *\r\n"
+            "Access-Control-Allow-Methods: POST\r\n"
+            "Access-Control-Allow-Headers: Content-Type\r\n"
+            "Content-Length: %zu\r\n"
+            "Connection: close\r\n\r\n",
+            code, status_text, body_len);
+
+        if (header_len <= 0 || (size_t)header_len >= sizeof(header))
+        {
+            log_msg(1, "Header formatting error\n");
+            return ERROR;
+        }
+
+        send(fd, header, header_len, 0);
+        send(fd, body, body_len, 0);
+    }
+    else
+    {
+        status_text = m_http_code_to_status_text(code);
+
+        body_len = snprintf(body_buf, sizeof(body_buf), "{\"error\": \"%s\"}", status_text);
+
+        header_len = snprintf(header, sizeof(header),
+            "HTTP/1.1 %d %s\r\n"
+            "Content-Type: application/json\r\n"
+            "Access-Control-Allow-Origin: *\r\n"
+            "Access-Control-Allow-Methods: POST\r\n"
+            "Access-Control-Allow-Headers: Content-Type\r\n"
+            "Content-Length: %zu\r\n"
+            "Connection: close\r\n\r\n",
+            code, status_text, body_len);
+
+        if (header_len <= 0 || (size_t)header_len >= sizeof(header))
+        {
+            log_msg(1, "Header formatting error\n");
+            return ERROR;
+        }
+
+        send(fd, header, header_len, 0);
+        send(fd, body_buf, body_len, 0);
+    }
+
+    log_msg(0, "Response sent to fd=%d with code %d\n", fd, code);
+    return SUCCESS;
+}
 
 void router_add(const char* path, route_cb_t cb, void* user_data)
 {
@@ -50,22 +133,67 @@ void router_clear()
     }
 }
 
+int router_parse_http_request(const char* request, size_t request_len, char** method, char** route, char** headers, char** body)
+{
+    const char* first_line_end;
+    const char* line_start;
+    const char* line_end;
+    const char* space1;
+    const char* space2;
+    const char* headers_end;
+    const char* body_start;
+    size_t headers_len;
+    size_t line_len;
+
+    if (!request || request_len == 0 || !method || !route || !headers || !body)
+        return ERROR;
+
+    first_line_end = strstr(request, "\r\n");
+    if (!first_line_end)
+        return ERROR;
+
+    line_start = request;
+    line_end = first_line_end;
+    line_len = line_end - line_start;
+
+    space1 = memchr(line_start, ' ', line_len);
+    if (!space1 || space1 >= line_end)
+        return ERROR;
+
+    *method = strndup(line_start, space1 - line_start);
+
+    space2 = memchr(space1 + 1, ' ', line_end - space1 - 1);
+    if (!space2 || space2 >= line_end)
+        return ERROR;
+
+    *route = strndup(space1 + 1, space2 - (space1 + 1));
+
+    headers_end = strstr(first_line_end + 2, "\r\n\r\n");
+    if (!headers_end)
+        return ERROR;
+
+    headers_len = headers_end - (first_line_end + 2);
+    *headers = strndup(first_line_end + 2, headers_len);
+
+    body_start = headers_end + 4;
+    if ((size_t)(body_start - request) < request_len)
+        *body = strndup(body_start, request + request_len - body_start);
+    else
+        *body = NULL;
+
+    return SUCCESS;
+}
+
 int router_handle_http_request(int fd, const char* request, size_t request_len)
 {
-    const char* method = NULL;
     const char* route = NULL;
-    const char* headers = NULL;
-    const char* body = NULL;
-    const char* headers_end = NULL;
     const char* first_line_end = NULL;
     char first_line[256] = {0};
-    char* err_string = NULL;
     char* space_pos = NULL;
-    size_t headers_len = 0;
+    char *route_end = NULL;
     size_t first_line_len = 0;
     route_entry_t* route_entry = NULL;
-    
-    (void)request_len;
+    http_request_ctx_t request_ctx;
 
     first_line_end = strstr(request, "\r\n");
     if (first_line_end)
@@ -76,88 +204,32 @@ int router_handle_http_request(int fd, const char* request, size_t request_len)
         space_pos = strchr(first_line, ' ');
         if (space_pos)
         {
-            method = first_line;
             *space_pos = '\0';
             route = space_pos + 1;
 
-            char *route_end = strchr(route, ' ');
+            route_end = strchr(route, ' ');
             if (route_end)
-            {
                 *route_end = '\0';
-            }
         }
     }
 
     if (!route)
-    {
-        log_msg(LOG_LEVEL_ERROR, "Invalid HTTP request: No route found\n");
-        err_string = "HTTP/1.1 400 Bad Request\r\nContent-Type: text/plain\r\n\r\nBad Request\r\n";
-        send(fd, err_string, strlen(err_string), 0);
-        log_msg(LOG_LEVEL_ERROR, "Response sent to fd=%d\n", fd);
-        return ERROR;
-    }
+        return router_http_generate_response(fd, CODE_400_BAD_REQUEST, "{\"error\": \"Bad Request\"}");
 
     route_entry = router_find(route);
     if (!route_entry)
-    {
-        log_msg(LOG_LEVEL_ERROR, "No route found for '%s'\n", route);
-        err_string = "HTTP/1.1 404 Not Found\r\nContent-Type: text/plain\r\n\r\nNot Found\r\n";
-        send(fd, err_string, strlen(err_string), 0);
-        log_msg(LOG_LEVEL_ERROR, "Response sent to fd=%d\n", fd);
-        return ERROR;
-    }
+        return router_http_generate_response(fd, CODE_404_NOT_FOUND, "{\"error\": \"Not Found\"}");
 
-    headers_end = strstr(request, "\r\n\r\n");
-    if (headers_end)
-    {
-        headers_len = headers_end - (first_line_end + 2);
-        headers = (char *)malloc(headers_len + 1);
-        strncpy((char *)headers, first_line_end + 2, headers_len);
-        ((char *)headers)[headers_len] = '\0';
-    }
+    if (!route_entry->handler)
+        return router_http_generate_response(fd, CODE_500_INTERNAL_SERVER_ERROR, "{\"error\": \"Internal Server Error\"}");
 
-    if (headers_end)
-    {
-        body = headers_end + 4;
-    }
+    /* Populate request ctx */
+    request_ctx.fd = fd;
+    request_ctx.request = request;
+    request_ctx.request_len = request_len;
 
-    log_msg(LOG_LEVEL_INFO, "Parsed HTTP request:");
-    log_msg(LOG_LEVEL_INFO, "Method:\n\t- '%s'\n", method ? method : "Unknown");
-    log_msg(LOG_LEVEL_INFO, "Route:\n\t- '%s'\n", route ? route : "Unknown");
-    log_msg(LOG_LEVEL_INFO, "Body:\n\t- '%s'\n", body ? body : "None");
-    log_msg(LOG_LEVEL_ERROR, "%s\n", request);
-
-    if (method && (strcmp(method, "POST") != 0) && (strcmp(method, "PUT") != 0))
-    {
-        log_msg(LOG_LEVEL_ERROR, "Unsupported HTTP method: %s\n", method ? method : "Unknown");
-        err_string = "HTTP/1.1 405 Method Not Allowed\r\nContent-Type: text/plain\r\n\r\nMethod Not Allowed\r\n";
-        send(fd, err_string, strlen(err_string), 0);
-        log_msg(LOG_LEVEL_ERROR, "Response sent to fd=%d\n", fd);
-        if (headers)
-        {
-            free((char *)headers);
-        }
-        return ERROR;
-    }
-
-    if (headers)
-    {
-        free((char *)headers);
-    }
-
-    const char *response =
-    "HTTP/1.1 200 OK\r\n"
-    "Access-Control-Allow-Origin: *\r\n"
-    "Access-Control-Allow-Methods: POST\r\n"
-    "Access-Control-Allow-Headers: Content-Type\r\n"
-    "Content-Type: text/plain\r\n"
-    "Connection: close\r\n"
-    "\r\n"
-    "Thanks for the report.\n";
-
-    send(fd, response, strlen(response), 0);
-
-    log_msg(LOG_LEVEL_INFO, "Response sent to fd=%d\n", fd);
+    /* call the handler */
+    route_entry->handler(&request_ctx, route_entry->user_data);
 
     return SUCCESS;
 }
--- server/srcs/router/router_api.h
+++ server/srcs/router/router_api.h
@@ -8,10 +8,6 @@ typedef struct
     int fd;
     const char* request;
     size_t request_len;
-    const char* method;
-    const char* route;
-    const char* headers;
-    const char* body;
 } http_request_ctx_t;
 
 typedef void (*route_cb_t)(http_request_ctx_t* request_ctx, void *user_data);
@@ -24,10 +20,25 @@ typedef struct route_entry
     UT_hash_handle hh;
 } route_entry_t;
 
+typedef enum
+{
+    CODE_200_OK = 200,
+    CODE_201_CREATED = 201,
+    CODE_204_NO_CONTENT = 204,
+    CODE_400_BAD_REQUEST = 400,
+    CODE_404_NOT_FOUND = 404,
+    CODE_405_METHOD_NOT_ALLOWED = 405,
+    CODE_500_INTERNAL_SERVER_ERROR = 500,
+    CODE_503_SERVICE_UNAVAILABLE = 503
+} HTTP_response_code_t;
+
 void router_add(const char* path, route_cb_t cb, void* user_data);
 void router_delete(route_entry_t* entry);
 void router_clear();
 
 int router_handle_http_request(int fd, const char* request, size_t request_len);
+int router_http_generate_response(int fd, HTTP_response_code_t code, const char* body);
+int router_parse_http_request(const char* request, size_t request_len, char** method,\
+    char** route, char** headers, char** body);
 
 #endif /* ROUTER_API_H */
