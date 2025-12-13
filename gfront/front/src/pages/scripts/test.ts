1ca6f6b1a939b6f0401d062809a61768c1a969f0
Guillem Forns <132559287+GuillemFo@users.noreply.github.com>
2025-12-13 08:56:30 +0000

    basic carousel

--- gfront/front/src/pages/scripts/test.ts
+++ gfront/front/src/pages/scripts/test.ts
@@ -4,8 +4,8 @@ import { createBtn, createDiv, createImage } from "./cssTools";
 initPage("test", () => {
   const testContainer = document.getElementById("test-container");
   if (testContainer) {
-    let test_img = ["https://images.unsplash.com/photo-1595871465907-19020bb76ad1?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D","https://images.unsplash.com/photo-1587584867790-cf443d163e87?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D","https://images.unsplash.com/photo-1593839686924-4b344fac3f8f?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"];
-    testContainer.appendChild(carousel_reusable("test", test_img, "matchcar"));
+	let test_img = ["https://images.unsplash.com/photo-1595871465907-19020bb76ad1?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D","https://images.unsplash.com/photo-1587584867790-cf443d163e87?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D","https://images.unsplash.com/photo-1593839686924-4b344fac3f8f?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"];
+	testContainer.appendChild(carousel_reusable("test", test_img, "matchcar"));
   }
 });
 
@@ -16,63 +16,73 @@ initPage("test", () => {
 
 
 
-// Basic reusable carousel hehe
+// Basic reusable carousel
 // missing dot navigation and responsive
-// write the css in each place without depending of .css file
+// write the css in each place without depending of .css file ??
 function carousel_reusable(name: string, img: string[], css_name: string)
 {
-    const carousel = createDiv(css_name + "-wrap");
-    const carousel_track = createDiv(css_name + "-track");
-    
+	const carousel = createDiv(css_name + "-wrap");
+	const carousel_track = createDiv(css_name + "-track");
+	
 	let currentIndex = 0;
 	let i = 0;
-    img.forEach(src => 
-    {
-      const carousel_slide = createDiv(css_name + "-slide");
-      const carousel_img = createImage(src, "img_" + i, "profile_" + i, css_name + "-img"); // check if multiple img with same name will colide...
-      carousel_slide.appendChild(carousel_img);
-      carousel_track.appendChild(carousel_slide);
-      i++;
-    });
+	img.forEach(src => 
+	{
+	  const carousel_slide = createDiv(css_name + "-slide");
+	  const carousel_img = createImage(src, "img_" + i, "profile_" + i, css_name + "-img"); // check if multiple img with same name will colide...
+	  carousel_slide.appendChild(carousel_img);
+	  carousel_track.appendChild(carousel_slide);
+	  i++;
+	});
 
-    
-    const carousel_btn_prev = createBtn("",name + "-prev", css_name + "-prev");
-
-    const carousel_btn_next = createBtn("",name + "-next", css_name + "-next");
-
-    carousel.appendChild(carousel_track);
-    carousel.appendChild(carousel_btn_prev);
-    carousel.appendChild(carousel_btn_next);
 	
-	//Read, understand and apply
-	function updateCarousel() {
-        const slideWidth = carousel.clientWidth;
-        carousel_track.style.transform = `translateX(-${currentIndex * slideWidth}px)`;
+	const carousel_btn_prev = createBtn("",name + "-prev", css_name + "-prev");
+
+	const carousel_btn_next = createBtn("",name + "-next", css_name + "-next");
+
+	carousel.appendChild(carousel_track);
+	carousel.appendChild(carousel_btn_prev);
+	carousel.appendChild(carousel_btn_next);
+	
+	//https://www.w3schools.com/jsref/prop_element_clientwidth.asp
+	//https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_element_clientheight
+	
+	function updateCarousel()
+	{
+		const slideWidth = carousel.clientWidth;
+		carousel_track.style.transform = `translateX(-${currentIndex * slideWidth}px)`;
 	}
 	
-    
-    function nextSlide() {
-        if (currentIndex < img.length - 1) {
-            currentIndex++;
-        } else {
-            currentIndex = 0; // Loop back to start
-        }
-        updateCarousel();
-    }
+	
+	function nextSlide()
+	{
+		if (currentIndex < img.length - 1)
+		{
+			currentIndex++;
+		}
+		else
+		{
+			currentIndex = 0;
+		}
+		updateCarousel();
+	}
 
-	function prevSlide() {
-        if (currentIndex > 0) {
-            currentIndex--;
-        } else {
-            currentIndex = img.length - 1; // Loop to end
-        }
-        updateCarousel();
-    }
-    
-    carousel_btn_next.onclick = nextSlide;
-    carousel_btn_prev.onclick = prevSlide;
+	function prevSlide()
+	{
+		if (currentIndex > 0)
+		{
+			currentIndex--;
+		}
+		else
+		{
+			currentIndex = img.length - 1;
+		}
+		updateCarousel();
+	}
+	
+	carousel_btn_next.onclick = nextSlide;
+	carousel_btn_prev.onclick = prevSlide;
 
-    return carousel;
+	return carousel;
 }
 
-//https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_element_clientheight
